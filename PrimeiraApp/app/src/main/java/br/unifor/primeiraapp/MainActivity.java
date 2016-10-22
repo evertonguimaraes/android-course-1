80c7bc93e3c800e782e8efb59bab5eed7157ac0b
Bruno Lopes <brunolopesjn@gmail.com>
2016-10-22 16:56:33 -0300

    Exemplo de como abrir uma activity através de uma intent explícita

--- PrimeiraApp/app/src/main/java/br/unifor/primeiraapp/MainActivity.java
+++ PrimeiraApp/app/src/main/java/br/unifor/primeiraapp/MainActivity.java
@@ -1,18 +1,21 @@
 package br.unifor.primeiraapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnLongClickListener {
 
     private TextView mTextViewMensagem;
     private TextView mTextViewResposta;
     private EditText mEditTextNome;
     private Button mButtonCliqueAqui;
+    private Button mMainButtonWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +34,61 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mMainButtonWelcome = (Button) findViewById(R.id.main_button_welcome);
+        mMainButtonWelcome.setOnLongClickListener(this);
+
+        Log.i("App", "método onCreate() chamado");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("App", "método onRestart() chamado");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("App", "método onStart() chamado");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("App", "método onResume() chamado");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("App", "método onPause() chamado");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("App", "método onStop() chamado");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("App", "método onDestroy() chamado");
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+
+        switch (v.getId()){
+            case R.id.main_button_welcome:
+
+                Intent it = new Intent(this, SecondaryActivity.class);
+                startActivity(it);
+
+                break;
+        }
+
+        return false;
     }
 }
---
+++ PrimeiraApp/app/src/main/java/br/unifor/primeiraapp/SecondaryActivity.java
@@ -0,0 +1,14 @@
+package br.unifor.primeiraapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary);
+    }
+
+}
