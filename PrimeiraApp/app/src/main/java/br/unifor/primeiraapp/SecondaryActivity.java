fcd12d65b80266b7d2538089b402e7b2fb16442c
evertonguimaraes <eguimaraes@unifor.br>
2016-10-22 16:15:26 -0300

    Modificações
    Inclusão de modificações no layout;
    Inclusão de nova Activity;
    Modificação do arquivo de layout.

---
+++ PrimeiraApp/app/src/main/java/br/unifor/primeiraapp/SecondaryActivity.java
@@ -0,0 +1,13 @@
+package br.unifor.primeiraapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary);
+    }
+}
